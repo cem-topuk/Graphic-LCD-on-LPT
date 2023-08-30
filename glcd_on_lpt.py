8b42d6fd0cc7b387f24a258813bee44278d95683
Cem Topuk <143652489+cem-topuk@users.noreply.github.com>
2023-08-30 23:06:47 +0300

    Add files via upload

---
+++ glcd_on_lpt.py
@@ -0,0 +1,314 @@
+import parallel
+#import time
+
+T6963CFG_SET_CURSOR_POINTER =          0x21
+T6963CFG_SET_OFFSET_REGISTER =         0x22
+T6963CFG_SET_ADDRESS_POINTER =         0x24
+
+T6963CFG_SET_TEXT_HOME_ADDRESS =       0x40
+T6963CFG_SET_TEXT_AREA =               0x41
+T6963CFG_SET_GRAPHIC_HOME_ADDRESS =    0x42
+T6963CFG_SET_GRAPHIC_AREA =            0x43
+
+T6963CFG_MODE_SET =                    0x80
+T6963CFG_EXT_CG_MODE =                 0x08
+T6963CFG_OR_MODE =                     0x00
+T6963CFG_XOR_MODE =                    0x01
+T6963CFG_AND_MODE =                    0x03
+TEXT_ATTR_MODE =                       0x04
+
+
+T6963CFG_DISPLAY_MODE =                0x90
+T6963CFG_CURSOR_BLINK_ON =             0x01
+T6963CFG_CURSOR_DISPLAY_ON =           0x02
+T6963CFG_TEXT_DISPLAY_ON =             0x04
+T6963CFG_GRAPHIC_DISPLAY_ON =          0x08
+
+T6963CFG_CURSOR_PATTERN_SELECT =       0xA0
+T6963CFG_CURSOR_1_LINE =               0x00
+T6963CFG_CURSOR_2_LINE =               0x01
+T6963CFG_CURSOR_3_LINE =               0x02
+T6963CFG_CURSOR_4_LINE =               0x03
+T6963CFG_CURSOR_5_LINE =               0x04
+T6963CFG_CURSOR_6_LINE =               0x05
+T6963CFG_CURSOR_7_LINE =               0x06
+T6963CFG_CURSOR_8_LINE =               0x07
+
+T6963CFG_SET_DATA_AUTO_WRITE =         0xB0
+T6963CFG_SET_DATA_AUTO_READ =          0xB1
+T6963CFG_AUTO_RESET =                  0xB2
+
+T6963CFG_DATA_WRITE_AND_INCREMENT =    0xC0
+T6963CFG_DATA_READ_AND_INCREMENT =     0xC1
+T6963CFG_DATA_WRITE_AND_DECREMENT =    0xC2
+T6963CFG_DATA_READ_AND_DECREMENT =     0xC3
+T6963CFG_DATA_WRITE_AND_NONVARIALBE =  0xC4
+T6963CFG_DATA_READ_AND_NONVARIABLE =   0xC5
+
+T6963CFG_SCREEN_PEEK =                 0xE0
+T6963CFG_SCREEN_COPY =                 0xE8
+
+GLCD_NUMBER_OF_LINES =              128
+GLCD_PIXELS_PER_LINE =              240
+GLCD_FONT_WIDTH =                   8   # Depends on the state of the FS pin L = 8, H = 6
+
+GLCD_GRAPHIC_AREA =                 int(GLCD_PIXELS_PER_LINE / GLCD_FONT_WIDTH)
+GLCD_TEXT_AREA =                    int(GLCD_PIXELS_PER_LINE / GLCD_FONT_WIDTH)
+GLCD_GRAPHIC_SIZE =                 GLCD_GRAPHIC_AREA * GLCD_NUMBER_OF_LINES
+GLCD_TEXT_SIZE =                    GLCD_TEXT_AREA * int(GLCD_NUMBER_OF_LINES / 8)
+
+GLCD_TEXT_HOME =                    0
+GLCD_GRAPHIC_HOME =                 GLCD_TEXT_HOME + GLCD_TEXT_SIZE
+
+GLCD_OFFSET_REGISTER =              2
+GLCD_EXTERNAL_CG_HOME =             int(GLCD_OFFSET_REGISTER) << 11
+GLCD_EXTERNAL_CG_CAPABILITY =       256 #Byte
+
+#DELAY_TIME =                        0.00000005 # 50 ns
+
+class LCD:
+    def __init__(self):
+        self.p = parallel.Parallel()
+        self.GLCD_init()
+
+    def GLCD_init(self):
+        self.p.setInitOut(0)        # LPT Pin 16  - LCD Pin 10          NOT Reset
+        self.p.setSelect(1)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        self.p.setAutoFeed(0)       # LPT Pin 14 - LCD Pin 8            C/Not D
+        #time.sleep(DELAY_TIME)
+        self.p.setDataDir(0)
+        self.p.setData(0)
+        self.p.setInitOut(1)        # LPT Pin 16  - LCD Pin 10          NOT Reset
+
+        self.GLCD_write(int(GLCD_TEXT_HOME & 0xFF), 0)
+        self.GLCD_write(int(GLCD_TEXT_HOME >> 8), 0)
+        self.GLCD_write(T6963CFG_SET_TEXT_HOME_ADDRESS, 1)
+
+        self.GLCD_write(GLCD_TEXT_AREA, 0)
+        self.GLCD_write(0, 0)
+        self.GLCD_write(T6963CFG_SET_TEXT_AREA, 1)
+
+        self.GLCD_write(int(GLCD_GRAPHIC_HOME & 0x00FF), 0)
+        self.GLCD_write(int(GLCD_GRAPHIC_HOME >> 8), 0)
+        self.GLCD_write(T6963CFG_SET_GRAPHIC_HOME_ADDRESS, 1)
+
+        self.GLCD_write(GLCD_GRAPHIC_AREA, 0)
+        self.GLCD_write(0, 0)
+        self.GLCD_write(T6963CFG_SET_GRAPHIC_AREA, 1)
+
+        self.GLCD_write(GLCD_OFFSET_REGISTER, 0)
+        self.GLCD_write(0, 0)
+        self.GLCD_write(T6963CFG_SET_OFFSET_REGISTER, 1)
+
+        self.GLCD_write(0, 0)
+        self.GLCD_write(0, 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+        self.GLCD_write(T6963CFG_DISPLAY_MODE  | T6963CFG_GRAPHIC_DISPLAY_ON   | T6963CFG_TEXT_DISPLAY_ON , 1)#| T6963CFG_CURSOR_DISPLAY_ON, 1)
+
+        self.GLCD_write(T6963CFG_MODE_SET | T6963CFG_OR_MODE, 1)
+
+    def check_status(self):
+        self.p.setSelect(1)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        self.p.setDataDir(0)        # Read Mode
+        self.p.setDataStrobe(0)     # LPT Pin 1 - LCD Pin 5 and 6       W/Not R
+        self.p.setAutoFeed(1)       # LPT Pin 14 - LCD Pin 8            C/Not D
+        #time.sleep(DELAY_TIME)
+        self.p.setSelect(0)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        #time.sleep(DELAY_TIME)
+        self.sta_check = 0
+        while True:
+            self.data = self.p.getData()
+            self.sta_check = self.data & 0x03
+            #time.sleep(DELAY_TIME)
+            if self.sta_check == 3:
+                break
+        #time.sleep(DELAY_TIME)
+        self.p.setSelect(1)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        #time.sleep(DELAY_TIME)
+
+    def GLCD_write(self, data, c_d):    # If c_d is 1, it is command mode, and 0 is data mode.
+        self.check_status()
+        self.p.setDataDir(1)        # Write Mode
+        self.p.setAutoFeed(c_d)     # LPT Pin 14 - LCD Pin 8            C/Not D
+        self.p.setDataStrobe(1)     # LPT Pin 1 - LCD Pin 5 and 6       W/Not R
+        self.p.setData(int(data))
+        #time.sleep(DELAY_TIME)
+        self.p.setSelect(0)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        #time.sleep(DELAY_TIME)
+        self.p.setSelect(1)         # LPT Pin 17 - LCD Pin 7            NOT CE
+
+    def GLCD_read(self, c_d):   # If c_d is 1, it is command mode, and 0 is data mode.
+        self.check_status()
+        self.p.setAutoFeed(c_d)     # LPT Pin 14 - LCD Pin 8            C/Not D
+        self.p.setDataStrobe(0)     # LPT Pin 1 - LCD Pin 5 and 6       W/Not R
+        self.p.setSelect(0)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        #time.sleep(DELAY_TIME)
+        data = self.p.getData()
+        #time.sleep(DELAY_TIME)
+        self.p.setSelect(1)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        return data
+
+    def GLCD_clear_text(self):
+        self.GLCD_write(GLCD_TEXT_HOME, 0)
+        self.GLCD_write(GLCD_TEXT_HOME >> 8, 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+        for i in range(0, GLCD_TEXT_SIZE):
+            self.GLCD_write(0, 0)
+            self.GLCD_write(T6963CFG_DATA_WRITE_AND_INCREMENT, 1)
+
+    def GLCD_clear_CG(self):
+        self.GLCD_write(GLCD_EXTERNAL_CG_HOME & 0xFF, 0)
+        self.GLCD_write(GLCD_EXTERNAL_CG_HOME >> 8, 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+        for i in range(0, GLCD_EXTERNAL_CG_CAPABILITY*8):
+            self.GLCD_write(0, 0)
+            self.GLCD_write(T6963CFG_DATA_WRITE_AND_INCREMENT, 1)
+
+    def GLCD_clear_graphic(self):
+        self.GLCD_write(GLCD_GRAPHIC_HOME & 0xFF, 0)
+        self.GLCD_write(GLCD_GRAPHIC_HOME >> 8, 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+        for i in range(0, GLCD_GRAPHIC_SIZE):
+            self.GLCD_write(0, 0)
+            self.GLCD_write(T6963CFG_DATA_WRITE_AND_INCREMENT, 1)
+
+    def GLCD_write_char(self, ch):
+        self.GLCD_write(ch - 32, 0)
+        self.GLCD_write(T6963CFG_DATA_WRITE_AND_INCREMENT, 1)
+    
+    def GLCD_write_string(self, text):
+        for char in text:
+            self.GLCD_write_char(ord(char))
+
+    def GLCD_set_cursor_pointer(self, x, y):
+        self.GLCD_write(x, 0)
+        self.GLCD_write(y, 0)
+        self.GLCD_write(T6963CFG_SET_CURSOR_POINTER, 1)
+
+    def GLCD_set_offset_register(self, data):
+        self.GLCD_write(data, 0)
+        self.GLCD_write(0, 0)
+        self.GLCD_write(T6963CFG_SET_OFFSET_REGISTER, 1)
+
+    def GLCD_set_address_pointer(self, x, y):
+        self.address = GLCD_TEXT_HOME +  x + (GLCD_TEXT_AREA * y)
+        self.GLCD_write(int(self.address & 0xFF), 0)
+        self.GLCD_write(int(self.address >> 8), 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+    def GLCD_set_pixel(self, x, y, color):
+        self.address = int(GLCD_GRAPHIC_HOME + (x / GLCD_FONT_WIDTH) + (GLCD_GRAPHIC_AREA * y))
+
+        self.GLCD_write(int(self.address & 0x00FF), 0)
+        self.GLCD_write(int(self.address >> 8), 0)
+        self.GLCD_write(T6963CFG_SET_ADDRESS_POINTER, 1)
+
+        self.GLCD_write(T6963CFG_DATA_READ_AND_NONVARIABLE, 1)
+        tmp = self.GLCD_read(0)
+
+        if color:
+            tmp |= (1 <<  (GLCD_FONT_WIDTH - 1 - (x % GLCD_FONT_WIDTH)))
+        else:
+            tmp &= ~(1 <<  (GLCD_FONT_WIDTH - 1 - (x % GLCD_FONT_WIDTH)))
+
+        self.GLCD_write(tmp, 0)
+        self.GLCD_write(T6963CFG_DATA_WRITE_AND_INCREMENT, 1)
+
+    def GLCD_close(self):
+        self.GLCD_clear_text()
+        self.GLCD_clear_graphic()
+        self.p.setInitOut(0)        # LPT Pin 16  - LCD Pin 10          NOT Reset
+        self.p.setSelect(0)         # LPT Pin 17 - LCD Pin 7            NOT CE
+        self.p.setData(0)
+        self.p.setDataStrobe(0)     # LPT Pin 1 - LCD Pin 5 and 6       W/Not R
+        self.p.setAutoFeed(0)       # LPT Pin 14 - LCD Pin 8            C/Not D
+        #time.sleep(DELAY_TIME)
+        self.p.setInitOut(1)        # LPT Pin 16  - LCD Pin 10          NOT Reset
+
+    def GLCD_draw_rectangle(self, x, y, b, a):
+        color = 1
+        for i in range(0, int(a)):
+            self.GLCD_set_pixel(int(x), int(y+i), color)
+            self.GLCD_set_pixel(int(x+b-1), int(y+i), color)
+        for i in range(0, int(b)):
+            self.GLCD_set_pixel(int(x+i), int(y), color)
+            self.GLCD_set_pixel(int(x+i), int(y+a-1), color)
+
+    def GLCD_draw_circle(self, cx, cy ,radius):
+        x = int(radius)
+        y = 0
+        xchange = 1 - 2 * int(radius)
+        ychange = 1
+        radiusError = 0
+        color = 1
+        while x >= y:
+            self.GLCD_set_pixel(int(cx)+x, int(cy)+y, color)
+            self.GLCD_set_pixel(int(cx)-x, int(cy)+y, color)
+            self.GLCD_set_pixel(int(cx)-x, int(cy)-y, color)
+            self.GLCD_set_pixel(int(cx)+x, int(cy)-y, color)
+            self.GLCD_set_pixel(int(cx)+y, int(cy)+x, color)
+            self.GLCD_set_pixel(int(cx)-y, int(cy)+x, color)
+            self.GLCD_set_pixel(int(cx)-y, int(cy)-x, color)
+            self.GLCD_set_pixel(int(cx)+y, int(cy)-x, color)
+            y += 1
+            radiusError += ychange
+            ychange += 2
+            if 2 * radiusError + xchange > 0:
+                x -= 1
+                radiusError += xchange
+                xchange += 2
+
+    def GLCD_draw_line(self, x1, y1, x2, y2):
+        color = 1
+        TwoDxAccumulatedError = 0
+        TwoDyAccumulatedError = 0
+
+        Dx = int(x2-x1)
+        Dy = int(y2-y1)
+
+        TwoDx = int(Dx + Dx)
+        TwoDy = int(Dy + Dy)
+
+        CurrentX = int(x1)
+        CurrentY = int(y1)
+
+        Xinc = 1
+        Yinc = 1
+
+        if Dx < 0:
+            Xinc = -1
+            Dx = -Dx
+            TwoDx = -TwoDx
+
+        if Dy < 0:
+            Yinc = -1
+            Dy = -Dy
+            TwoDy = -TwoDy
+
+        self.GLCD_set_pixel(int(x1), int(y1), color)
+
+        if Dx != 0 and Dy != 0:
+
+            if Dy <= Dx:
+                TwoDxAccumulatedError = 0
+                while CurrentX != x2:
+                    CurrentX += Xinc
+                    TwoDxAccumulatedError += TwoDy
+                    if TwoDxAccumulatedError > Dx:
+                        CurrentY += Yinc
+                        TwoDxAccumulatedError -= TwoDx
+                    self.GLCD_set_pixel(CurrentX, CurrentY, color)
+            else:
+                TwoDyAccumulatedError = 0
+                while CurrentY != y2:
+                    CurrentY += Yinc
+                    TwoDyAccumulatedError += TwoDx
+                    if TwoDyAccumulatedError > Dy:
+                        CurrentX += Xinc
+                        TwoDyAccumulatedError -= TwoDy
+                    self.GLCD_set_pixel(CurrentX, CurrentY, color)
+
---
+++ ip_test.py
@@ -0,0 +1,65 @@
+import glcd_on_lpt as glol
+import time
+import subprocess
+import re
+
+class LINUX_sys:
+    def __init__(self):
+        pass
+    def get_ip(self, device):
+        result = subprocess.run(['ip', 'a'], stdout=subprocess.PIPE, text=True)
+
+        output_lines = result.stdout.splitlines()
+
+        ip_pattern = re.compile(r'inet (\d+\.\d+\.\d+\.\d+)/\d+ brd')#re.compile(r'inet (\d+\.\d+\.\d+\.\d+)')
+        ip_address = None
+        device_counter = 0
+        for line in output_lines:
+            if line.startswith(f"{device}:"):
+                device_counter += 1
+            if device_counter == 1 and "inet" in line:
+                match = ip_pattern.search(line)
+                if match:
+                    ip_address = match.group(1)
+                    break
+        if ip_address:
+            return ip_address
+        else:
+            return '0'
+        
+if __name__ == "__main__":
+    lcd = glol.LCD()
+    lcd.GLCD_clear_text()
+    lcd.GLCD_clear_graphic()
+    linux = LINUX_sys()
+    ip_address = linux.get_ip(3)
+    print(f"{ip_address}")
+    
+    # Draw some graphics
+    lcd.GLCD_draw_rectangle((240/2)-50, (128/2)-50, 100, 100)
+    lcd.GLCD_draw_circle(240/2, 128/2, 45)
+    lcd.GLCD_draw_line(0, 0, 240, 128)
+    lcd.GLCD_draw_line(0, 128, 240, 0)
+
+    time.sleep(2)
+    lcd.GLCD_clear_graphic()
+    lcd.GLCD_set_address_pointer(0, 1)
+    lcd.GLCD_write_string(f'LAN IP: {ip_address}')
+    #time.sleep(2)
+    #lcd.GLCD_set_address_pointer(0, 0)
+    #lcd.GLCD_write_string("###Hello, LCD!***")
+    #time.sleep(2)
+    #lcd.GLCD_clear_text()
+    """
+    # Demo test numbers
+    for count in range(0, 147):
+        ch = f'{count}'
+        #lcd.GLCD_write_char(ord(ch[0]))
+        lcd.GLCD_write_string(f'{count} ')
+        timer.sleep(0.1)
+    """
+    while True:
+        user_input = input("'q' for exit: ")
+        if user_input.lower() == 'q':
+            lcd.GLCD_close()
+            break
